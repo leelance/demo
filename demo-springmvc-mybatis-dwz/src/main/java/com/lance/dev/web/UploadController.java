c8814b963247e5bb0602fd712591a22334d929b6
lihao <li6151770@126.com>
2015-05-17 23:26:45 +0800

    修改上传文件common-fileupload

--- demo-springmvc-mybatis-dwz/src/main/java/com/lance/dev/web/UploadController.java
+++ demo-springmvc-mybatis-dwz/src/main/java/com/lance/dev/web/UploadController.java
@@ -1,15 +1,21 @@
 package com.lance.dev.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
-
+	private Logger logger = LogManager.getLogger(getClass());
+	
 	@ResponseBody
 	@RequestMapping("upload")
-	public String upload() {
-		return null;
+	public String upload(@RequestParam MultipartFile dir) {
+		logger.debug("fileName: {}", dir.getOriginalFilename());
+		return "{\"error\":0, \"url\":\"filePath\"}";
 	}
 }

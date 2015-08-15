9e4ba7d380af74b6610e142b060d5d8c65570f76
lance <li6151770@126.com>
2015-08-15 23:45:27 +0800

    pom.xml add jpa persistent
    pom.xml add jpa persistent

---
+++ demo-lucene/src/main/java/com/lance/dev/entity/BaseEntity.java
@@ -0,0 +1,62 @@
+package com.lance.dev.entity;
+
+import java.util.Date;
+
+/**
+ * Created by lance
+ *
+ * @since 2015-08-15 23:29
+ */
+
+public abstract class BaseEntity {
+
+    private Integer id;
+
+    private String creator;
+
+    private Date createTime;
+
+    private String updator;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdator() {
+        return updator;
+    }
+
+    public void setUpdator(String updator) {
+        this.updator = updator;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}

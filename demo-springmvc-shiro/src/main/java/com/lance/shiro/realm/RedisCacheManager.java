0963364d2a83598dad5c58df00fc2cce957df654
lance <li6151770@126.com>
2016-06-04 23:39:27 +0800

    update redis cache
    update redis cache cachemanager

--- demo-springmvc-shiro/src/main/java/com/lance/shiro/realm/RedisCacheManager.java
+++ demo-springmvc-shiro/src/main/java/com/lance/shiro/realm/RedisCacheManager.java
@@ -1,13 +1,17 @@
 package com.lance.shiro.realm;
 
+import org.apache.shiro.cache.AbstractCacheManager;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
-import org.apache.shiro.cache.CacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
-public class RedisCacheManager implements CacheManager {
+public class RedisCacheManager extends AbstractCacheManager {
+	@Autowired
+	private RedisTemplate<byte[], Object> redisTemplate;
 
 	@Override
-	public <K, V> Cache<K, V> getCache(String name) throws CacheException {
-		return null;
+	protected Cache<byte[], Object> createCache(String name) throws CacheException {
+		return new RedisCache<byte[], Object>(redisTemplate, name);
 	}
 }

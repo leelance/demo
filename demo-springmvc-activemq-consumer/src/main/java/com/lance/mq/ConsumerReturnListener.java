94015d22e8b7ba19e320882303e3667692f7345d
lance <li6151770@126.com>
2016-06-12 13:05:38 +0800

    update consumer listener
    update consumer listener,  listener message reply

---
+++ demo-springmvc-activemq-consumer/src/main/java/com/lance/mq/ConsumerReturnListener.java
@@ -0,0 +1,28 @@
+package com.lance.mq;
+
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.jms.listener.SessionAwareMessageListener;
+
+import com.alibaba.fastjson.JSON;
+
+public class ConsumerReturnListener implements SessionAwareMessageListener<TextMessage> {
+	Logger logger = LogManager.getLogger();
+
+	@Override
+	public void onMessage(TextMessage message, Session session) throws JMSException {
+		logger.info("message: {}", JSON.toJSONString(message));
+		
+		//返回收到消息后通知
+		MessageProducer producer = session.createProducer(null);
+		TextMessage replyMessage = session.createTextMessage();
+		replyMessage.setText("SUCCESS");
+		replyMessage.setJMSCorrelationID(message.getJMSCorrelationID());
+		producer.send(message.getJMSReplyTo(), replyMessage);
+	}
+}

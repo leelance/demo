748c1cbf9f4fa3d7fdaad0077220f53869497bd2
lance <li6151770@126.com>
2016-06-16 15:09:05 +0800

    add watcher zookeeper
    add watcher zookeeper

---
+++ demo-springmvc-zookeeper/src/main/java/com/lance/zk/ZooKeeperWatcher.java
@@ -0,0 +1,12 @@
+package com.lance.zk;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ZooKeeperWatcher implements Watcher {
+
+	@Override
+	public void process(WatchedEvent event) {
+
+	}
+}

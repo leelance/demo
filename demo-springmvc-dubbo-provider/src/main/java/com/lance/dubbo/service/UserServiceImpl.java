6720f401d1ed3e38e4e65f46e130fb2cd14af77c
lance <li6151770@126.com>
2016-06-20 13:40:52 +0800

    update userservice class
    update userservice class

--- demo-springmvc-dubbo-provider/src/main/java/com/lance/dubbo/service/UserServiceImpl.java
+++ demo-springmvc-dubbo-provider/src/main/java/com/lance/dubbo/service/UserServiceImpl.java
@@ -11,25 +11,21 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public UserInfo findOne(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<UserInfo> findByAccount(String account) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void update(UserInfo info) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void save(UserInfo info) {
-		// TODO Auto-generated method stub
 
 	}
 }
